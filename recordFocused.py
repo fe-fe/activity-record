ad99d1ca13a6e15db72802da18a1aef80c16152f
fefe <119269054+fe-fe@users.noreply.github.com>
2025-02-19 16:52:10 -0300

    save data function
    store tags and respective time active in activity.json
    checks for changes every 5 seconds

--- recordFocused.py
+++ recordFocused.py
@@ -3,18 +3,22 @@ from time import sleep
 from getpass import getuser
 import os
 import re
+from datetime import datetime
+import json
 
 tags = [
-    # tags that must be tracked
+    # one-tags that must be tracked
     # if the index is an array, the first index is the word to be matched 
     # and the second is the one to be shown
-    "python", ["py", "python"], "java", "selenium", "javascript", ["js", "javascript"], "django", "flask",
-    "flutter", "bootstrap", "spring", ["nlp", "natural language processing"], "html", "css", "git",
-    "github", "react", "angular", "linkedin", "stack overflow",
+    "python", ["py", "python"], "java", "selenium", "javascript", ["js", "javascript"], 
+    "flutter", "bootstrap", "spring", ["nlp", "natural language processing"], "flask",
+    ["pln", "natural language processing"], "html", "css", "git", "django",
+    "github", "react", "angular", "linkedin", "json", ["ts", "typescript"]
     
 ]
 
 multiTags = [
+
     "visual studio code", "stack overflow", "natural language processing" 
 ]
 
@@ -31,8 +35,8 @@ def checkForIntelliJ():
 def checkForTags(title):
     # checks if the title of the window has matching tags
     # will append a value only one time, no repetition
-    # if the matching tag is a list, the second index will be append if the first index is positive
-    # returns a list of tags that matched
+    # multi-word tags required context so they are checked before chopping the title
+    # one-word tags require precision so they are checked after chopping the title
     matches = []
     title = title.lower()
 
@@ -57,10 +61,44 @@ def getFocus():
     return tags # extracts data from the title
 
 
+def writeAcitivity(tags, time):
+    try:
+        with open("activity.json", "r") as activity:
+            data:dict = json.load(activity)
+    except:
+        data = {}
+
+    for tag in tags:
+        tag = re.sub(" ", "_", tag)
+        hours = data.get(tag) # int or None
+        if not hours:
+            hours = 0
+        data[tag] = hours + time
+
+    result = json.dumps(data, indent=4)
+
+    with open("activity.json", "w") as activity:
+        activity.write(result)
+
+
 checkForIntelliJ()
 
-i=0
-while i < 10:
-    print(getFocus())
-    i+=1
-    sleep(4)
+while True:
+
+    # the current window's data (tags) is extracted
+    # and the current time is stored in "start"
+    # the program sleeps until the program identifies changes in the tags
+    # and then it writes the tags and how much time in hours they have been positive 
+
+    current = getFocus()
+    start = datetime.now()
+    sleep(5)
+    
+    while current == getFocus():
+        sleep(5)
+    
+    elapsed = datetime.now() - start 
+    elapsed = (elapsed.total_seconds())/3600
+    writeAcitivity(current, elapsed)
+
+     
