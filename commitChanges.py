a8be331a4f79804732a13a0d4355a1431de3c16e
fefe <119269054+fe-fe@users.noreply.github.com>
2025-02-19 20:17:26 -0300

    function to wipe the data every week

--- commitChanges.py
+++ commitChanges.py
@@ -3,6 +3,7 @@ import requests
 import base64
 import os
 import json
+from datetime import datetime
 
 load_dotenv()
 
@@ -31,13 +32,36 @@ def loadActivity():
     return data
 
 
+def isWipeDay(today: datetime):
+
+    with open("activity.json", "r") as activity:
+        data = dict(json.load(activity))
+        lastWipe = data.get("last_wipe")
+
+    # the activity hasn't been wiped today and today is sunday 
+    if lastWipe != str(today.date()) and today.weekday() == 0:
+        with open("activity.json", "w") as activity:
+            data = {"last_wipe": str(today.date())}
+            activity.write(json.dumps(data, indent=4)) # writes only the last wipe day
+        return True
+    else:
+        return False
+
+
 def commitActivity():
-    filecontent = loadActivity()
+
+    isWipeDay(datetime.today())
+    fileContent = loadActivity()
     sha = getFileSHA()
+
     data = {
         "message": "activity updated by api",
-        "content": filecontent,
+        "content": fileContent,
         "sha": sha
     }
+
     response = requests.put(api_url, headers=headers, data=json.dumps(data))
     return response.json(), response.ok
+
+
+commitActivity()
